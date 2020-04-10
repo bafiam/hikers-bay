03083a25b195961b8f59d590fb77eb2818b03547
bafiam <stevebafiam@gmail.com>
2020-04-10 19:52:58 +0300

    add relationship and user association and hacks

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,3 +1,4 @@
 class Opinion < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_10_122625) do
+ActiveRecord::Schema.define(version: 2020_04_10_135237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2020_04_10_122625) do
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
