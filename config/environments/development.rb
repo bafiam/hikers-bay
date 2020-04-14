26b53890e13dfbf9ace56618fe7ac93d364e34d6
bafiam <stevebafiam@gmail.com>
2020-04-14 23:19:57 +0300

    remove unwanted files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,12 @@
 Rails.application.configure do
   config.after_initialize do
-      Bullet.enable        = true
-      Bullet.alert         = false
-      Bullet.bullet_logger = true
-      Bullet.console       = true
-    # Bullet.growl         = true
-      Bullet.rails_logger  = true
-      Bullet.add_footer    = false
+    #   Bullet.enable        = true
+    #   Bullet.alert         = false
+    #   Bullet.bullet_logger = true
+    #   Bullet.console       = true
+    # # Bullet.growl         = true
+    #   Bullet.rails_logger  = true
+    #   Bullet.add_footer    = false
     end  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
