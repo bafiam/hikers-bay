5ce40734a24f937afa2a551580c08a0999b2e56b
bafiam <stevebafiam@gmail.com>
2020-04-15 21:16:14 +0300

    add controller tests

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Opinion, type: :model do
-  context 'should belong to the' do
+  context 'Should belong to a' do
     it 'author' do
       should belong_to(:author).class_name('User').with_foreign_key('author_id')
     end
   end
-  context 'should have specific column. It has' do
+  context 'should have' do
     it 'a text column of string' do
       should have_db_column(:text).of_type(:string)
     end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  context 'should belong to the' do
+  context 'Should belong to a' do
     it 'follower' do
       should belong_to(:follower).class_name('User')
     end
@@ -9,7 +9,7 @@ RSpec.describe Relationship, type: :model do
       should belong_to(:followed).class_name('User')
     end
   end
-  context 'should have specific column. It has' do
+  context 'Should have ' do
     it 'a follower_id column of integer' do
       should have_db_column(:follower_id).of_type(:integer)
     end
